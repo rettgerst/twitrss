db43fccf6a17845236b05a084dc7f80cce82c48c
rett gerst <rett@apago.com>
2021-05-13 21:23:32 -0400

    constrain media width

--- functions/renderToXml.tsx
+++ functions/renderToXml.tsx
@@ -29,59 +29,95 @@ function getTweetMediaTable(tweet: Tweet) {
 
 	const length = mediaEntities.length;
 
-	if (length === 1) return <img src={mediaEntities[0].media_url_https} />;
-	else if (length === 2)
-		return (
-			<table>
-				<tr>
-					<td>
-						<img src={mediaEntities[0].media_url_https} />
-					</td>
-					<td>
-						<img src={mediaEntities[1].media_url_https} />
-					</td>
-				</tr>
-			</table>
-		);
-	else if (length === 3)
-		return (
-			<table>
-				<tr>
-					<td>
-						<img src={mediaEntities[0].media_url_https} />
-					</td>
-					<td rowSpan={2}>
-						<img src={mediaEntities[2].media_url_https} />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={mediaEntities[1].media_url_https} />
-					</td>
-				</tr>
-			</table>
-		);
-	else if (length === 4)
-		return (
-			<table>
-				<tr>
-					<td>
-						<img src={mediaEntities[0].media_url_https} />
-					</td>
-					<td>
-						<img src={mediaEntities[1].media_url_https} />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<img src={mediaEntities[2].media_url_https} />
-					</td>
-					<td>
-						<img src={mediaEntities[3].media_url_https} />
-					</td>
-				</tr>
-			</table>
-		);
+	let width = 600;
+
+	if (length === 1)
+		return <img width={width} src={mediaEntities[0].media_url_https} />;
+	else {
+		let tableContents;
+
+		if (length === 2)
+			tableContents = (
+				<>
+					<tr>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[0].media_url_https}
+							/>
+						</td>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[1].media_url_https}
+							/>
+						</td>
+					</tr>
+				</>
+			);
+		else if (length === 3)
+			tableContents = (
+				<>
+					<tr>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[0].media_url_https}
+							/>
+						</td>
+						<td rowSpan={2}>
+							<img
+								width={width / 2}
+								src={mediaEntities[2].media_url_https}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[1].media_url_https}
+							/>
+						</td>
+					</tr>
+				</>
+			);
+		else if (length === 4)
+			tableContents = (
+				<>
+					<tr>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[0].media_url_https}
+							/>
+						</td>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[1].media_url_https}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[2].media_url_https}
+							/>
+						</td>
+						<td>
+							<img
+								width={width / 2}
+								src={mediaEntities[3].media_url_https}
+							/>
+						</td>
+					</tr>
+				</>
+			);
+
+		return <table width={400}>{tableContents}</table>;
+	}
 }
 
 export default function renderToXml(tweets: Tweet[]) {
